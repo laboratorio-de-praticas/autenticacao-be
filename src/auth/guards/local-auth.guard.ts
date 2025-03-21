bf7845634c4455faa1115833b80e19c2a2e84c8d
Vinicius Leal Alves <leealalves@hotmail.com>
2025-03-21 02:20:37 -0300

    feature: local strategy now verify body inputs

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { UserLoginRequestDto } from '../dto/user-login-request.dto';
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const userLoginRequestDto = plainToInstance(
+      UserLoginRequestDto,
+      request.body,
+    );
+    const errors = await validate(userLoginRequestDto);
+    if (errors.length > 0) {
+      const formattedErrors = errors.map((err) =>
+        Object.values(err.constraints).join(' '),
+      );
+
+      throw new BadRequestException({
+        statusCode: 400,
+        error: 'Bad Request',
+        message: formattedErrors,
+      });
+    }
+
+    return super.canActivate(context) as Promise<boolean>;
+  }
+}
